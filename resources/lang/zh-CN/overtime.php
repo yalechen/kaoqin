63cee0c90589360149796e619c4ca42f458f31c1
jois <wyxychenzhijiang@163.com>
2015-07-09 22:53:05 +0800

    一些label的实现，待完善

--- resources/lang/zh-CN/overtime.php
+++ resources/lang/zh-CN/overtime.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'status' => [
-        \App\Models\Overtime::STATUS_PASS => '审核通过',
-        \App\Models\Overtime::STATUS_REJECT => '审核驳回',
-        \App\Models\Overtime::STATUS_WAIT => '待审核中'
+        \App\Models\Overtime::STATUS_PASS => '通过',
+        \App\Models\Overtime::STATUS_REJECT => '驳回',
+        \App\Models\Overtime::STATUS_WAIT => '待审'
     ]
 ];
